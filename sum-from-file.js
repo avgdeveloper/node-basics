63f530013613c6805859ead2bdaf764679dacc6f
Aviad Gintzler <aviad@medisonmedia.com>
2026-06-17 03:47:14 +0300

    sum from file - some changes

--- sum-from-file.js
+++ sum-from-file.js
@@ -1,5 +1,10 @@
 import fs from "fs"
 
+
+sumFromFile('data/nums.txt')
+    .then(sum => console.log('Sum:', sum))
+    .catch(err => console.log('Cannot sum:', err))
+
 function sumFromFile(filePath) {
     return new Promise((resolve, reject) => {
         fs.readFile(filePath, 'utf8', (err, data) => {
@@ -15,7 +20,3 @@ function sumFromFile(filePath) {
         })
     })
 }
-
-sumFromFile('data/nums.txt')
-    .then(sum => console.log('Sum:', sum))
-    .catch(err => console.log('Cannot sum:', err))
